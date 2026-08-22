41e6278806928835310efa286600365ffc408ba7
dev-marieclaire <mail.maryjamesb@gmail.com>
2026-08-21 20:06:39 -0600

    Update in delta: correcting stored unit in delta_clock_time().

--- engine/src/core/delta.c
+++ engine/src/core/delta.c
@@ -10,5 +10,5 @@ void delta_time_init(delta_t *delta_time, unsigned int target_timelapse)
 { delta_time->current = delta_store_time() / target_timelapse; }
 
 // Stores the elapsed time in miliseconds since the program's execution.
-unsigned int delta_store_time(void)
-{ return (unsigned int) clock() / CLOCKS_PER_SEC; }
+unsigned int delta_clock_time(void)
+{ return (unsigned int) (clock() * 1000) / CLOCKS_PER_SEC; }
