9c8e5700baa7d21e808b9cb2545ac6a149409600
dev-marieclaire <mail.maryjamesb@gmail.com>
2026-08-22 12:56:34 -0600

    Fixing Makefiles.

--- engine/platform_config.mk
+++ engine/platform_config.mk
@@ -2,16 +2,18 @@ ifeq ($(TARGET),OLD)
 	# If installed as documented in the repo's readme.md, this must work.
 	DJGPP_PREFIX ?= /usr/local/djgpp
 	CC	:= $(DJGPP_PREFIX)/bin/i586-pc-msdosdjgpp-gcc
-
+	CXX	:= $(DJGPP_PREFIX)/bin/i586-pc-msdosdjgpp-g++
 	CFLAGS	= -I$(INCDIR) -D__OLD__ -DALLEGRO_NO_MAGIC_MAIN
+	CXXFLAGS = $(CFLAGS)
 	LDFLAGS	= -L$(NADIA_ROOTDIR)/../lib/allegro/djgpp -lalleg
-	PLATFORM_SRC	:= $(wildcard $(SRCDIR)/old/*.c)
+	PLATFORM_SRC := $(shell find $(SRCDIR)/platform/old -type f \( -name "*.c" -o -name "*.cpp" \))
 endif
 
 ifeq ($(TARGET),MODERN)
-	CC	= g++
+	CC	= gcc
+	CXX	= g++
 	CFLAGS	= -I$(INCDIR) -D__MODERN__ `sdl2-config --cflags`
+	CXXFLAGS = $(CFLAGS)
 	LDFLAGS	= `sdl2-config --libs` -lSDL2_image -lSDL2_ttf -lm
-	PLATFORM_SRC	:= $(wildcard $(SRCDIR)/modern/*.c)
+	PLATFORM_SRC := $(shell find $(SRCDIR)/platform/modern -type f \( -name "*.c" -o -name "*.cpp" \))
 endif
-
